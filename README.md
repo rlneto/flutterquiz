7fe627fa824fc2d8a57d3a606e4b0709b89be466
Rudolfo Lange Neto <rlneto@live.com>
2025-05-01 17:56:55 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+#Flutter Quiz
+
+## Descrição
+
+Mobile App em flutter que apresenta um quiz com perguntas e respostas.
---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import "package:flutter/material.dart";
+import "package:flutterquiz/start_screen.dart";
+
+void main() {
+  runApp(const MaterialApp(home: Scaffold(body: StartScreen())));
+}
