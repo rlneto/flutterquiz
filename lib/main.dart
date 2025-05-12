6a0364c445fe4ac8448df9388021464a304feaca
Rudolfo Lange Neto <rlneto@live.com>
2025-05-12 06:07:31 -0300

    Tela inicial proto 1

--- lib/main.dart
+++ lib/main.dart
@@ -2,5 +2,30 @@ import "package:flutter/material.dart";
 import "package:flutterquiz/start_screen.dart";
 
 void main() {
-  runApp(const MaterialApp(home: Scaffold(body: StartScreen())));
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        body: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              begin: Alignment.topCenter,
+              end: Alignment.bottomCenter,
+              colors: <Color>[
+                Color(0xff1f005c),
+                Color(0xff5b0060),
+                Color(0xff870160),
+                Color(0xffac255e),
+                Color(0xffca485c),
+                Color(0xffe16b5c),
+                Color(0xfff39060),
+                Color(0xffffb56b),
+              ],
+              tileMode: TileMode.mirror,
+            ),
+          ),
+          child: const StartScreen(),
+        ),
+      ),
+    ),
+  );
 }
--- lib/start_screen.dart
+++ lib/start_screen.dart
@@ -2,9 +2,29 @@ import 'package:flutter/material.dart';
 
 class StartScreen extends StatelessWidget {
   const StartScreen({super.key});
+  final image = 'assets/images/quiz-logo.png';
 
   @override
   Widget build(context) {
-    return const Text('Tela Inicial');
+    return Center(
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Image.asset(image, width: 300, color: Colors.white54),
+          const SizedBox(height: 80),
+          const Text(
+            'Aprender com diversão!',
+            style: TextStyle(color: Colors.white, fontSize: 24),
+          ),
+          const SizedBox(height: 30),
+          OutlinedButton.icon(
+            onPressed: () {},
+            style: OutlinedButton.styleFrom(foregroundColor: Colors.white),
+            icon: Icon(Icons.arrow_right_alt),
+            label: const Text('Começar agora!'),
+          ),
+        ],
+      ),
+    );
   }
 }
