66c669923ae65b2c9e97bce29052dc9124186250
Rudolfo Lange Neto <rlneto@live.com>
2025-05-22 05:33:49 -0300

    Layout inicial de perguntras e respostas

--- lib/questions_screen.dart
+++ lib/questions_screen.dart
@@ -12,6 +12,19 @@ class QuestionsScreen extends StatefulWidget {
 class _QuestionsScreenState extends State<QuestionsScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Text('Tela de Perguntas');
+    return SizedBox(
+      width: double.infinity,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text('Pergunta', style: TextStyle(color: Colors.white)),
+          SizedBox(height: 30),
+          ElevatedButton(onPressed: () {}, child: Text('Resposta 1')),
+          ElevatedButton(onPressed: () {}, child: Text('Resposta 2')),
+          ElevatedButton(onPressed: () {}, child: Text('Resposta 3')),
+          ElevatedButton(onPressed: () {}, child: Text('Resposta 4')),
+        ],
+      ),
+    );
   }
 }
