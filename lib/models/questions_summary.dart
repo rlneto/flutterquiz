32c19ab37fd8b705200e137dd4823f49e6cb040b
Rudolfo Lange Neto <rlneto@live.com>
2025-05-23 05:21:44 -0300

    Formatação dos resultados

--- lib/models/questions_summary.dart
+++ lib/models/questions_summary.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class QuestionsSummary extends StatelessWidget {
   const QuestionsSummary(this.summaryData, {super.key});
@@ -13,20 +14,63 @@ class QuestionsSummary extends StatelessWidget {
         child: Column(
           children:
               summaryData.map((data) {
-                return Row(
-                  children: [
-                    Text(((data['question_index'] as int) + 1).toString()),
-                    Expanded(
-                      child: Column(
-                        children: [
-                          Text(data['question'] as String),
-                          SizedBox(height: 5),
-                          Text(data['user_answer'] as String),
-                          Text(data['correct_answer'] as String),
-                        ],
+                final isCorrect = data['user_answer'] == data['correct_answer'];
+
+                return Container(
+                  margin: const EdgeInsets.only(bottom: 16),
+                  child: Row(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Container(
+                        width: 30,
+                        height: 30,
+                        alignment: Alignment.center,
+                        decoration: BoxDecoration(
+                          color:
+                              isCorrect
+                                  ? Colors.green.shade300
+                                  : Colors.red.shade300,
+                          borderRadius: BorderRadius.circular(15),
+                        ),
+                        child: Text(
+                          ((data['question_index'] as int) + 1).toString(),
+                          style: GoogleFonts.lato(
+                            color: Colors.white,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
                       ),
-                    ),
-                  ],
+                      const SizedBox(width: 16),
+                      Expanded(
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              data['question'] as String,
+                              style: GoogleFonts.lato(
+                                color: Colors.white,
+                                fontSize: 16,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                            const SizedBox(height: 4),
+                            Text(
+                              data['user_answer'] as String,
+                              style: GoogleFonts.lato(
+                                color: Colors.red.shade200,
+                              ),
+                            ),
+                            Text(
+                              data['correct_answer'] as String,
+                              style: GoogleFonts.lato(
+                                color: Colors.green.shade200,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
                 );
               }).toList(),
         ),
--- lib/results_screen.dart
+++ lib/results_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutterquiz/data/questions.dart';
 import 'package:flutterquiz/models/questions_summary.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class ResultsScreen extends StatelessWidget {
   const ResultsScreen({super.key, required this.chosenAnswers});
@@ -28,26 +29,67 @@ class ResultsScreen extends StatelessWidget {
     final numTotalQuestions = questions.length;
     final numCorrectQuestions =
         summaryData
-            .where((data) {
-              return data['correct_answer'] == data['user_answer'];
-            })
-            .toList()
+            .where((data) => data['correct_answer'] == data['user_answer'])
             .length;
 
-    return SizedBox(
-      width: double.infinity,
-      child: Container(
-        margin: EdgeInsets.all(40),
+    return Scaffold(
+      body: Container(
+        padding: const EdgeInsets.all(40),
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            colors: [Colors.deepPurple.shade800, Colors.deepPurple.shade600],
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+          ),
+        ),
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             Text(
-              'Vocẽ respondeu $numCorrectQuestions de $numTotalQuestions perguntas corretamente!',
+              'Resultado do Quiz',
+              style: GoogleFonts.lato(
+                color: Colors.white,
+                fontSize: 24,
+                fontWeight: FontWeight.bold,
+              ),
             ),
-            SizedBox(height: 30),
+            const SizedBox(height: 30),
+            Container(
+              padding: const EdgeInsets.all(20),
+              decoration: BoxDecoration(
+                color: Colors.black54,
+                borderRadius: BorderRadius.circular(16),
+              ),
+              child: Text(
+                'Você acertou $numCorrectQuestions de $numTotalQuestions perguntas!',
+                style: GoogleFonts.lato(color: Colors.white, fontSize: 18),
+                textAlign: TextAlign.center,
+              ),
+            ),
+            const SizedBox(height: 30),
             QuestionsSummary(summaryData),
-            SizedBox(height: 30),
-            TextButton(onPressed: () {}, child: Text('Recomeçar o Quiz!')),
+            const SizedBox(height: 30),
+            ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.white,
+                foregroundColor: Colors.deepPurple,
+                padding: const EdgeInsets.symmetric(
+                  horizontal: 24,
+                  vertical: 12,
+                ),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(20),
+                ),
+              ),
+              onPressed: () {},
+              child: Text(
+                'Recomeçar Quiz',
+                style: GoogleFonts.lato(
+                  fontSize: 16,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
           ],
         ),
       ),
